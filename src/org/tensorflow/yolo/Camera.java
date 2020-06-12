60086d6ac2f0aae6baa5fbd3b1883866cf876108
백지연 <beperserving@gmail.com>
2020-06-12 16:35:57 +0900

    Merge branch 'master' of https://lab.hanium.or.kr/20_hg003d/main
    # Conflicts:
    #	build.gradle
    #	src/org/tensorflow/yolo/setting/AppSetting.java

---
+++ src/org/tensorflow/yolo/Camera.java
@@ -0,0 +1,41 @@
+package org.tensorflow.yolo;
+
+public class Camera {
+    private String c_url;
+    private String c_word_e;
+
+    public String getC_url() {
+        return c_url;
+    }
+
+    public void setC_url(String c_url) {
+        this.c_url = c_url;
+    }
+
+    public String getC_word_e() {
+        return c_word_e;
+    }
+
+    public void setC_word_e(String c_word_e) {
+        this.c_word_e = c_word_e;
+    }
+
+    public String getC_word_k() {
+        return c_word_k;
+    }
+
+    public void setC_word_k(String c_word_k) {
+        this.c_word_k = c_word_k;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    private String c_word_k;
+    private String uid;
+}
