6b45b5b1630838c8bafff2c6b56e21d30e1be088
백지연 <beperserving@gmail.com>
2020-09-23 16:25:26 +0900

    지연_사진퀴즈 완성

---
+++ src/org/tensorflow/yolo/CameraQuizAnswerActivity.java
@@ -0,0 +1,76 @@
+package org.tensorflow.yolo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.tensorflow.yolo.setting.AppSetting;
+
+public class CameraQuizAnswerActivity extends AppCompatActivity {
+    RelativeLayout answerlayout;
+    TextView tvans;
+    ImageButton btn;
+    Intent intent2 = null;
+    Intent intent3 = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wordanswer);
+        answerlayout = (RelativeLayout) findViewById(R.id.answerlayout);
+
+        Window w = getWindow();
+        Navigation_Bar n = new Navigation_Bar();
+        n.HideNavigationBar(w);
+
+
+        tvans = (TextView) findViewById(R.id.tvans);
+        btn = (ImageButton) findViewById(R.id.btn);
+        Intent intent = getIntent();
+        Camera cquiz_answer = (Camera) intent.getSerializableExtra("cquiz_answer");
+        String clicked_word=intent.getExtras().getString("clicked_word");
+        int b_id=intent.getExtras().getInt("b_id");
+        answerlayout.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.wordcorrect));
+
+        if(cquiz_answer.getC_word_e().equals(clicked_word)){
+            // 정답입니다.
+            answerlayout.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.wordcorrect));
+
+        }else{
+            // 오답입니다.
+
+            tvans.setText(cquiz_answer.getC_word_e() + "의 뜻은 " + cquiz_answer.getC_word_k());
+            answerlayout.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.wordwrong));
+        }
+
+        btn.setOnClickListener(view -> {
+            AppSetting.cq_count++;
+                Log.v("cqCount=======", AppSetting.cq_count + "");
+                if (AppSetting.cq_count < 5) {
+                    Log.v("cq_countCount=======", AppSetting.cq_count + "");
+                    intent2 = new Intent(getApplicationContext(), CameraQuizActivity.class);
+                    startActivity(intent2);
+
+                } else if (AppSetting.cq_count == 5) {
+                    Toast.makeText(getApplicationContext(), "이동하자", Toast.LENGTH_LONG).show();
+
+                    Log.v("Sentence Quiz=======", "OK");
+                    intent3 = new Intent(getApplicationContext(), PlanetActivity1.class);
+                    intent.putExtra("b_id",1 );
+                    startActivity(intent3);
+                    AppSetting.cq_count=0;
+                    finish();
+                }
+
+            }
+        );
+
+    }
+}
--- src/org/tensorflow/yolo/setting/AppSetting.java
+++ src/org/tensorflow/yolo/setting/AppSetting.java
@@ -2,8 +2,11 @@ package org.tensorflow.yolo.setting;
 
 import android.graphics.Bitmap;
 
+import org.tensorflow.yolo.Camera;
 import org.tensorflow.yolo.model.BoxPosition;
 import org.tensorflow.yolo.model.Recognition;
+
+import java.util.ArrayList;
 
 public class AppSetting {
     // 백지연
@@ -22,6 +25,12 @@ public class AppSetting {
     public static String image_name;
     public static String means;
     public static String uarl;
+
+    public static int cq_count=0;
+    public static ArrayList<Camera> cqlist;
+    public static Camera cq1;
+    public static Camera cq2;
+    public static Camera cq3;
 
     // 민주희 : 가입자 정보
     public static String uid;
