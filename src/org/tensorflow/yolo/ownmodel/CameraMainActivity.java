0549532565cbb4e40f38decdd7e079246adaa6f1
백지연 <beperserving@gmail.com>
2020-10-09 02:08:19 +0900

    지연_사물인식단어장 DB 재연결 로직 수정. 캡처시 프리뷰 구현예정

--- src/org/tensorflow/yolo/ownmodel/CameraMainActivity.java
+++ src/org/tensorflow/yolo/ownmodel/CameraMainActivity.java
@@ -1,12 +1,20 @@
 package org.tensorflow.yolo.ownmodel;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -16,6 +24,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
 import com.wonderkiln.camerakit.CameraKitEventListener;
@@ -27,20 +44,39 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.tensorflow.yolo.Camera;
+import org.tensorflow.yolo.NetworkService;
 import org.tensorflow.yolo.R;
+import org.tensorflow.yolo.RetrofitSender;
 import org.tensorflow.yolo.setting.AppSetting;
 import org.tensorflow.yolo.view.CameraActivity;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class CameraMainActivity extends AppCompatActivity {
 
+    // DB
+    StorageReference mStorageRef;
+    String imageUrl;
+    NetworkService networkService;
+    String fuckUrl;
+
+    // 로딩
     public ProgressDialog pdialog;
     public Context mctk=CameraMainActivity.this;
 
+    Bitmap bitmap;
     private static final String MODEL_PATH = "comon_output_quant.tflite";
     private static final boolean QUANT = true;
     private static final String LABEL_PATH = "comon_model_labels.txt";
@@ -53,7 +89,8 @@ public class CameraMainActivity extends AppCompatActivity {
     private Button btnDetectObject, btnToggleCamera;
     private ImageView imageViewResult;
     private CameraView cameraView;
-
+    private Button btnSaveDB;
+    boolean captureFlag=true;
     private String english_word;
     private String korea_word;
     @Override
@@ -63,10 +100,18 @@ public class CameraMainActivity extends AppCompatActivity {
         cameraView = findViewById(R.id.cameraView);
         imageViewResult = findViewById(R.id.imageViewResult);
         textViewResult = findViewById(R.id.textViewResult);
+
         textViewResult.setMovementMethod(new ScrollingMovementMethod());
 
         btnToggleCamera = findViewById(R.id.btnToggleCamera);
         btnDetectObject = findViewById(R.id.btnDetectObject);
+        btnSaveDB=findViewById(R.id.btnSaveDB);
+        btnSaveDB.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+               saveView();
+            }
+        });
 
         cameraView.addCameraKitListener(new CameraKitEventListener() {
             @Override
@@ -82,12 +127,9 @@ public class CameraMainActivity extends AppCompatActivity {
             @Override
             public void onImage(CameraKitImage cameraKitImage) {
 
-                Bitmap bitmap = cameraKitImage.getBitmap();
-
-                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
-
+                bitmap = cameraKitImage.getBitmap();
                 imageViewResult.setImageBitmap(bitmap);
-
+                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
                 final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
 
                 // 지연 : result들 중 퍼센트가 가장 높은 1개만 처리
@@ -95,6 +137,8 @@ public class CameraMainActivity extends AppCompatActivity {
                 english_word=(max.split(" "))[1];
                 JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
                 jsoupAsyncTask.execute();
+
+
 
 
             }
@@ -115,8 +159,26 @@ public class CameraMainActivity extends AppCompatActivity {
         btnDetectObject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cameraView.captureImage();
+
+                if(captureFlag){ //
+                    btnSaveDB.setVisibility(View.VISIBLE);
+                    btnDetectObject.setBackgroundColor(Color.YELLOW);
+                    btnDetectObject.setText("다시하기");
+                    cameraView.captureImage();
+
+
+                }else{
+                    textViewResult.setVisibility(View.INVISIBLE);
+                    btnSaveDB.setVisibility(View.INVISIBLE);
+                    btnDetectObject.setText("캡처하기");
+                    btnDetectObject.setBackgroundColor(Color.RED);
+                    captureFlag=true;
+                    imageViewResult.setVisibility(View.INVISIBLE);
+
+                }
+
             }
+
         });
 
         initTensorFlowAndLoadModel();
@@ -186,6 +248,7 @@ public class CameraMainActivity extends AppCompatActivity {
         String means="";
         @Override
         protected Void doInBackground(Void... params) {
+
             try {
                 String htmlPageUrl="https://dic.daum.net/search.do?type=eng&q="+ english_word;
                 Document doc = Jsoup.connect(htmlPageUrl).get();
@@ -206,16 +269,82 @@ public class CameraMainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void result) {
-            korea_word=means;
+            //imageViewResult.setVisibility(View.VISIBLE);
 
-            Toast.makeText(CameraMainActivity.this, english_word, Toast.LENGTH_SHORT).show();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            korea_word=means;
+            Toast.makeText(CameraMainActivity.this, korea_word, Toast.LENGTH_SHORT).show();
             textViewResult.setText(english_word+"\n"+korea_word);
             pdialog.dismiss();
+
+            textViewResult.setVisibility(View.VISIBLE);
+            Log.d("뭐지",textViewResult.getVisibility()+" ");
+
+
+            Log.d("뭐지",textViewResult.getVisibility()+" ");
+            captureFlag=false;
+        }
+    }
+    // 파이어베이스 연결
+    private void saveView()
+    {
+        if(bitmap!=null){
+            mStorageRef= FirebaseStorage.getInstance().getReference("Images/"+english_word);
+            Bitmap image_bitmap = bitmap; //?????????????????????????????/
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            image_bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            UploadTask uploadTask = mStorageRef.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(CameraMainActivity.this,"실패",Toast.LENGTH_LONG).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // FIREBASE STORAGE 이미지 저장
+                    // 성공하면
+                    Toast.makeText(CameraMainActivity.this,"저장 되었습니다. . .",Toast.LENGTH_LONG).show();
+
+                    Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+                            return mStorageRef.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                networkService = RetrofitSender.getNetworkService();
+                                // MYSQL CAMERA TABLE DB 저장
+                                Call<Camera> response = networkService.register_camera(task.getResult().toString(),english_word,korea_word,AppSetting.uid);
+                                response.enqueue(new Callback<Camera>() {
+                                    @Override
+                                    public void onResponse(Call<Camera> call, Response<Camera> response) {
+                                        Log.d("카메라","성공================================================================");
+                                    }
+                                    @Override
+                                    public void onFailure(Call<Camera> call, Throwable t) {
+                                        Log.d("카메라","실패================================================================");
+                                    }
+                                });
+                                fuckUrl=imageUrl;
+                                AppSetting.uarl=imageUrl;
+                                Log.e("fuckUrl"+fuckUrl,"==============================================");
+                            } else {
+                                Log.d("이미지","실패");
+                            }
+                        }
+                    });
+
+                }
+            });
+
+
         }
     }
 
